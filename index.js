82cc31dea01162e70b96310625f44c3564fab7d4
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-16 13:16:46 +0200

    addhtml

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require('http');
+const express = require('express');
+const app = new express();
+
+app.get('/', function(request, response){
+    response.sendFile('test.html');
+});
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end('<html><head><style> body {background-image: url('https://cswesteurope1003bffda3ce.blob.core.windows.net/images/lockscreen.jpg');}</style></head><body><p><b><font size="400" color="blue">Created with the power of Siri and Bicep!</font></b></p></body></html>')
+    response.end()
 
 const port = process.env.PORT || 1337;
 server.listen(port);
