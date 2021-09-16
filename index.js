b897cfcfa09579790ab4fab89a51ab876a779de0
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-16 12:28:45 +0200

    adddate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write({ now: new Date() });
     response.end("Created with the power of Siri and Bicep!");
 });
 
