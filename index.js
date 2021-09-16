4a330ae4caca84121bebc7f71523cb77e81cfdb6
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-16 12:49:06 +0200

    addbckgrnd

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Created with the power of Siri and Bicep!"{ now: new Date() });
-});
+    response.end('<html><body><p>"Created with the power of Siri and Bicep!<style>div {background-image: url('https://cswesteurope1003bffda3ce.blob.core.windows.net/images/lockscreen.jpg');}</style></p></body></html>');});
 
 const port = process.env.PORT || 1337;
 server.listen(port);
