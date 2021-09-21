301c8e299048b461d5fb40f91245ce777fc31ff9
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 15:08:05 +0200

    html5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
-	fs.readFile('./index.html', null, function (error, data) {
+	fs.readFile('/home/site/wwwroot/index.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
@@ -14,7 +14,7 @@ const server = http.createServer((request, response) => {
     response.end();
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
