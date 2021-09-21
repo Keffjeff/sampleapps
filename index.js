05ece151b3a7612100f59728e20186152ef899f6
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 15:31:20 +0200

    html9

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-let http = require('http');
-let fs = require('fs');
-
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
-
-http.createServer(handleRequest).listen(8080);
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
