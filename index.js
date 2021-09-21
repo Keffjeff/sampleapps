66e18b610f78405cf5225b4632f91644c931a150
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 15:21:25 +0200

    html7

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+let http = require('http');
+let fs = require('fs');
 
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-	fs.readFile('./index.html', null, function (error, data) {
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
         } else {
             response.write(data);
-        }});
-    response.end();
-});
+        }
+        response.end();
+    });
+};
 
-const port = process.env.PORT || 8080;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+http.createServer(handleRequest).listen(8080);
