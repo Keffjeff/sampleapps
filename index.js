7225da37897a6cba0d6e70aee77e7e1ba417293c
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 15:17:49 +0200

    html6

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
-	fs.readFile('/home/site/wwwroot/index.html', null, function (error, data) {
+	fs.readFile('./index.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
         } else {
             response.write(data);
-        }
+        }});
     response.end();
 });
 
