b8fc1863c145501fc48b903aaa43c06f3d702d98
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 14:53:21 +0200

    html4

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.end('<font size="400" color="blue">Created with the power of Siri and Bicep!</font>');
+	fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+    response.end();
 });
 
 const port = process.env.PORT || 1337;
