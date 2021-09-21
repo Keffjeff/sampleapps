da0f400d1205a9529b5a39fdf17994fc0bfc32bc
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 14:36:43 +0200

    html2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end(<font size="400" color="blue">Created with the power of Siri and Bicep!</font>);
 });
 
 const port = process.env.PORT || 1337;
