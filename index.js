b93e0b5b0b4ff6c3b05c81834fe165147a359282
Tommy Aronsson <tommy.aronsson@gmail.com>
2021-09-21 15:36:20 +0200

    html11

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('/home/wwwroot/index.html', function(err, data) {
+  fs.readFile('/home/site/wwwroot/index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
